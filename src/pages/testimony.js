dd4ecb761919ee8462036b6ef6020b8eb7658a34
Epi Martinez <epimartinez25@gmail.com>
2021-06-12 06:28:06 -0400

    Created src folder/component folder/navbar folder/index.js file/Navbar file. Also created pages folder/about.js file/index.js file/ testiony.js file/treatment.js file.

---
+++ src/pages/testimony.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Testimony = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+            >
+                <h1>Testimonies</h1>
+            </div>
+    );
+};
+
+export default Testimony;
---
+++ src/pages/treatments.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Treatments = () => {
+    return (
+        <div
+        style={{
+            display: 'flex',
+            justifyContent: 'Right',
+            alignItems: 'Right',
+            height: '100vh'
+        }}
+        >
+            <h1>Treatments</h1>
+        </div>
+    );
+};
+
+export default Treatments;
