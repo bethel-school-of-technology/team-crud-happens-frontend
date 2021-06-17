41f0675206ecd7477053fb6ed5e420845e4264cb
Epi Martinez <epimartinez25@gmail.com>
2021-06-16 21:05:41 -0400

    created rought draft dropdown menu on nnavbar in Navbar/index.js file.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,6 +5,7 @@ import {
     Bars,
     NavMenu,
 } from './NavbarElements';
+import { NavDropdown } from 'react-bootstrap';
 
 const Navbar = () => {
     return (
@@ -26,8 +27,14 @@ const Navbar = () => {
                     About
                 </NavLink>
                  {/* Second Nav */}
-                     
             </NavMenu>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
         </Nav>
         </>
     );
