d3c071bf802a9856ffc127196aa8ced8c2e0884f
ValdemirGoncalves <infojr.83@gmail.com>
2021-06-23 17:18:25 -0400

    firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDoJUGIzoH9rxEMCuBZsgkUH0RinnfeCMo",
+    authDomain: "stroke-survivors-53050.firebaseapp.com",
+    projectId: "stroke-survivors-53050",
+    storageBucket: "stroke-survivors-53050.appspot.com",
+    messagingSenderId: "431605611355",
+    appId: "1:431605611355:web:1ca0bc4576be975cb0387e"
+  };
+  firebase.initializeAppfirebase(firebaseConfig);
+  export default firebase;
