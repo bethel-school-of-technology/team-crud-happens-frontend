2c3406e721452a46d9c66d5c0c2d503266745bdc
Epi Martinez <epimartinez25@gmail.com>
2021-06-23 11:16:18 -0400

    I've made update to our pages, being responsive (showing on desktop, mobile app, etc). I had to do some changes and created additional files to frontend folder

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+.navbar {
+    background-color: #282c34;
+}
+
+a, .navbar-brand, .navbar-nav .nav-link {
+    color: white;
+
+    &:hover {
+        color: white;
+    }
+}
+`;
+
+export const NavigationBar = () => (
+    <Styles>
+        <Navbar expand="lg">
+            <Navbar.Brand href="/">Stroke Survivors</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Item><Nav.Link><Link to="/">Home</Link></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link><Link to="/testimony">Testimony</Link></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link><Link to="/treatments">Treatments</Link></Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link><Link to="/about">About</Link></Nav.Link></Nav.Item>
+                </Nav>
+                 </Navbar.Collapse>
+        </Navbar>
+    </Styles>
+
+)
--- src/pages/treatments.js
+++ src/pages/treatments.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
-const Treatments = () => {
-    return (
-        <div
-        style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'Right',
-            height: '100vh'
-        }}
-        >
-            <h1>Treatments</h1>
+export const Treatments = () => (
+        <div>
+        <h1>Treatments</h1>
         </div>
-    );
-};
+    )
+// import React from 'react';
 
-export default Treatments;
+// const Treatments = () => {
+//     return (
+//         <div
+//         style={{
+//             display: 'flex',
+//             justifyContent: 'center',
+//             alignItems: 'Right',
+//             height: '100vh'
+//         }}
+//         >
+//             <h1>Treatments</h1>
+//         </div>
+//     );
+// };
+
+// export default Treatments;
